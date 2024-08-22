08909296afb9bf087943235e020c988b7764209d
Daria Morel <moreld.m.2005@mail.ru>
2024-08-22 13:02:55 +0300

    урок 3

--- main.go
+++ main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
 }
 
 func (u User) getAllInfo() string {
-	return fmt.Sprintf("name: %s, age: %d, money: %d, grades: %f, happiness: %f", u.name, u.age, u.money, u.avg_grades, u.happiness)
+	return fmt.Sprintf("name: %s, age: %d, money: %d, grades: %f, happiness: %f", u.Name, u.Age, u.Money, u.Avg_grades, u.Happiness)
 }
 
-func home_page(page http.ResponseWriter, r *http.Request) {
-	bob := User{"Bob", 25, -50, 4.2, 0.8}
-	fmt.Fprintf(page, bob.getAllInfo()) // куда выводить и что выводить
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bob", 25, -50, 4.2, 0.8, []string{"Football", "Dancing"}}
+	// fmt.Fprintf(page, `<h1> Main text </h1> <b> main text </b>`) // куда выводить и что выводить
+	tmpl, _ := template.ParseFiles("templates/home_page.html") // откуда загружаем шаблон
+	tmpl.Execute(w, bob)                                       // страничка и что передаем внутрь шаблона
 }
 
 // http.ResponseWriter - параметр, позволяющий выводить что-то на страничку
